b65073da262d77b0ebb2d4d7fa3fc677be7515c9
Macktireh <abdimack97@gmail.com>
2025-08-03 13:32:29 +0200

    fix: adjust message item width for better responsiveness; update skeleton component layout

--- src/components/MessageSkeleton.tsx
+++ src/components/MessageSkeleton.tsx
@@ -3,9 +3,13 @@ export function MessageSkeleton() {
     <div className="flex flex-col gap-0.5">
       <div className="skeleton h-4 w-[100%]"></div>
       <div className="flex gap-0.5">
-        <div className="skeleton h-4 w-[40%]"></div>
-        <div className="skeleton h-4 w-[20%]"></div>
-        <div className="skeleton h-4 w-[40%]"></div>
+        <div className="skeleton h-4 w-[50%]"></div>
+        <div className="skeleton h-4 w-[50%]"></div>
+      </div>
+      <div className="flex gap-0.5">
+        <div className="skeleton h-4 w-[35%]"></div>
+        <div className="skeleton h-4 w-[30%]"></div>
+        <div className="skeleton h-4 w-[35%]"></div>
       </div>
       <div className="skeleton h-4 w-[50%]"></div>
     </div>
